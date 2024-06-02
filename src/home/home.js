66df60892c3b8e87a9dd24625b9cfa291d83fcad
Collin Dapper <collindapper@gmail.com>
2024-06-01 19:50:10 -0700

    Fixed search parameters

--- src/home/home.js
+++ src/home/home.js
@@ -12,10 +12,11 @@ class Home extends React.Component {
     this.state = {
       ipSearched: '',
       ipDetails: [],
-      lat: '22.5726',
-      lon: '88.3832',
-      error: '',
-      isSubmitted: false,
+      lat: '',
+      lon: '',
+      error: 'false',
+      errorReason: '',
+      isSubmitted: 'false',
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -23,7 +24,7 @@ class Home extends React.Component {
   }
 
   componentDidMount() {
-    this.setState({ isSubmitted: false });
+    this.setState({ isSubmitted: 'false' });
   }
 
   handleChange(event) {
@@ -43,26 +44,35 @@ class Home extends React.Component {
       .then(checkStatus)
       .then(json)
       .then((data) => {
-        console.log(Object.entries(data));
         
+        if (data.error === true) {
+          this.setState({
+            isSubmitted: 'true',
+            error: 'true',
+            errorReason: data.reason,
+            lat: 22.5726,
+            lon: 88.3832,
+          })
+        } else {
           this.setState({ 
+            isSubmitted: 'true',
+            error: 'false',
             ipDetails: data,
             lat: data.latitude,
-            lon: data.longitude,
-            error: '',
-            isSubmitted: true,
+            lon: data.longitude,            
           });
+        }
       })
       .catch((error) => {
-        this.setState({ error: error.message });
+        this.setState({ error: data.error });
         
-        console.log(error);
-      })   
+        console.log(data.error);
+      }) 
     }
 
   render() {
       // Set up initial state variables 
-      const { ipSearched, ipDetails, lat, lon, isSubmitted, error } = this.state;
+      const { ipSearched, ipDetails, lat, lon, isSubmitted, error, errorReason } = this.state;
 
     return (
       <div className='container homePage'>
@@ -73,12 +83,25 @@ class Home extends React.Component {
 
         {/* Search bar */}
         <form className='searchColumn d-flex justify-content-center' onSubmit={this.handleSubmit}>
-          <input className='input fontPrimary me-2' placeholder='Search IPv4 address' onChange={this.handleChange} value={ipSearched}></input>
-          <button type='submit' className='submitButton fontPrimary'>Find</button>
+          <input className='input fontPrimary me-2' placeholder='Search IPv4 or IPv6 address' onChange={this.handleChange} value={ipSearched}></input>
+          <button type='submit' className='submitButton fontPrimary'>Search</button>
         </form>
 
-        {/* Call searched results */}
-        {isSubmitted && !error ?
+        {/* Call searched results - ERROR */}
+        ({isSubmitted === 'true' && error === 'true' ?
+          <div className='container row d-flex justify-content-center mx-auto'>
+            <div className='column errorColumn col-12 pt-2'>
+              <h1 className='text-center text-danger'>ERROR!</h1>
+              <h3 className='text-center errorMessage'>{errorReason}</h3>
+            </div>
+          </div>
+          :
+          null
+        });
+
+
+        {/* Call searched results - SUCCESS */}
+        ({isSubmitted === 'true' && error === 'false' ?
             <div className='container row d-flex justify-content-around mx-auto'>
             <div className='column mapColumn col-12 col-md-5'>
               <SearchableMap lat={lat} lon={lon} />
@@ -97,7 +120,7 @@ class Home extends React.Component {
           </div>
           :
           null
-        }
+        });
       </div>
     )
   }
